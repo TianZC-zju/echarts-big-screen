06db223c240c68f5c7ed86dbc6dcef9e9b0f9642
tianzc <1149501789@qq.com>
2022-09-26 21:34:31 +0800

    feat: 搭好项目框架，同时自动适配不同宽高比的屏幕

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './home.scss';
 
 export const Home = () => {
+
+    const {clientWidth, clientHeight} = document.documentElement
+    const pageWidth = clientWidth/clientHeight > 16/9? clientHeight*16/9:clientWidth
+    const pageHeight = pageWidth *9 / 16
   return (
-    <div>
-      你好
-    </div>
+      <div className='container'>
+          <div className='page' style={{width:pageWidth,height:pageHeight}}>
+              你好
+          </div>
+      </div>
   );
 };
