b9b5b09db90c565ecf5ac7a12b3715a2492d3876
tianzc <1149501789@qq.com>
2022-09-26 22:58:48 +0800

    feat: 采用rem响应式布局，自动设置第一个div的宽高

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,13 +4,20 @@ import './home.scss';
 export const Home = () => {
 
     const {clientWidth, clientHeight} = document.documentElement
-    const pageWidth = clientWidth/clientHeight > 16/9? clientHeight*16/9:clientWidth
-    const pageHeight = pageWidth *9 / 16
-  return (
-      <div className='container'>
-          <div className='page' style={{width:pageWidth,height:pageHeight}}>
-              你好
-          </div>
-      </div>
-  );
+    const pageWidth = clientWidth / clientHeight > 16 / 9 ? clientHeight * 16 / 9 : clientWidth
+    const pageHeight = pageWidth * 9 / 16
+
+    let html = document.querySelector('html')
+    html.style.fontSize=pageWidth/100 + 'px'
+
+    return (
+        <div className='container'>
+            <div className='page' style={{width: pageWidth, height: pageHeight}}>
+                你好
+                <div className="x">
+                    第一个div
+                </div>
+            </div>
+        </div>
+    );
 };
