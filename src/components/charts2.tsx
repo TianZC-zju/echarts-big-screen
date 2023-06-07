73896e07696080b371e191afd5932cd2826770f8
ttc <1149501789@qq.com>
2023-06-07 16:20:11 +0800

    feat: 完成装机容量和发电量

--- src/components/charts2.tsx
+++ src/components/charts2.tsx
@@ -1,76 +1,70 @@
-import React, {useEffect, useRef} from 'react';
-import * as echarts from 'echarts';
-import chartTheme from '../shared/echartsTheme.json'
+import React, { useEffect, useRef } from "react";
+import * as echarts from "echarts";
+import chartTheme from "../shared/echartsTheme.json";
 
-import px from '../shared/px';
+import px from "../shared/px";
 
-const Charts2=()=>{
-
-    const option = {
-        tooltip: {
-            trigger: 'item'
+const Charts2 = () => {
+  const option = {
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      bottom: px(2),
+      // right:px(40),
+      orient: "vertical",
+      textStyle: { color: "white", fontSize: px(14) },
+      height: px(40),
+      width: px(50),
+    },
+    grid: [
+      {
+        top: px(10),
+        bottom: px(50),
+        left: px(60),
+        // containLabel: true,
+      },
+    ],
+    xAxis: {
+      //   show: false,
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: "rgba(255,255,255,0.1)",
         },
-        legend: {
-            bottom:px(2),
-            // right:px(40),
-            orient:'vertical',
-            textStyle:{color: 'white',fontSize:px(14)},
-            height:px(40),
-            width:px(50),
-        },
-        grid:[{
-            top:px(10),
-            bottom:px(50),
-            left:px(60),
-            // containLabel: true,
-        }],
-        xAxis: {
-            show:false,
-        },
-        yAxis: {
-            type: 'category',
-            data: ['类型1','类型2','类型3','类型4','类型5','类型6','类型7','类型8',],
-            splitLine:{show:false},
-            axisLabel:{fontSize:px(8),width:px(32),overflow:'break',interval:0},
-        },
-        series: [
-            {
-                name: '发电',
-                type: 'bar',
-                data: [1,1,1,1,1,1,1,1,1],
-                // itemStyle:{color:'#1440c9'},
-            },
-            {
-                name: '用电',
-                type: 'bar',
-                data: [2, 2, 2, 2, 2, 2, 2, 2, 2],
-                // itemStyle:{color:{
-                //         type: 'linear',
-                //         x: 0,
-                //         y: 0,
-                //         x2: 1,
-                //         y2: 0,
-                //         colorStops: [{
-                //             offset: 0, color: '#9223ba' // 0% 处的颜色
-                //         }, {
-                //             offset: 1, color: '#555ab9' // 100% 处的颜色
-                //         }],
-                //         global: false // 缺省为 false
-                //     }},
-            }
-        ]
-    };
-    const barRef = useRef(null)
-    useEffect(()=>{
-        echarts.registerTheme('virtualbox-power', chartTheme)
-        const myChart = echarts.init(barRef.current, 'virtualbox-power');
-        myChart.setOption(option);
-    },[])
+      },
+    },
+    yAxis: {
+      type: "category",
+      data: ["2014", "2015", "2016", "2017", "2018"],
+      splitLine: { show: false },
+      axisLabel: {
+        fontSize: px(12),
+        width: px(32),
+        overflow: "break",
+        interval: 0,
+      },
+    },
+    series: [
+      {
+        name: "发电",
+        type: "bar",
+        data: [5, 1, 3, 1.5, 2],
+      },
+      {
+        name: "装机",
+        type: "bar",
+        data: [6, 2, 1, 2.5, 3],
+      },
+    ],
+  };
+  const barRef = useRef(null);
+  useEffect(() => {
+    echarts.registerTheme("virtualbox-power", chartTheme);
+    const myChart = echarts.init(barRef.current, "virtualbox-power");
+    myChart.setOption(option);
+  }, []);
 
-    return(
-        <div ref={barRef} className="chart">
-
-        </div>
-    )
-}
-export default Charts2
+  return <div ref={barRef} className="chart"></div>;
+};
+export default Charts2;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,68 +1,66 @@
-import React from 'react';
-import './home.scss';
-import Charts1 from '../components/charts1';
-import Charts2 from '../components/charts2';
-import px from '../shared/px';
-import Charts3 from '../components/charts3_mock';
-import Charts4 from '../components/charts4';
-import Charts5 from '../components/charts5';
-import Charts6 from '../components/charts6';
-import Charts7 from '../components/charts7';
-import Charts8 from '../components/charts8';
-import Charts9 from '../components/charts9';
-import Charts10 from '../components/charts10';
-import VPContainer from '../components/container';
-import VPBorderBox from '../components/borderBox';
+import React from "react";
+import "./home.scss";
+import Charts1 from "../components/charts1";
+import Charts2 from "../components/charts2";
+import px from "../shared/px";
+import Charts3 from "../components/charts3_mock";
+import Charts4 from "../components/charts4";
+import Charts5 from "../components/charts5";
+import Charts6 from "../components/charts6";
+import Charts7 from "../components/charts7";
+import Charts8 from "../components/charts8";
+import Charts9 from "../components/charts9";
+import Charts10 from "../components/charts10";
+import VPContainer from "../components/container";
+import VPBorderBox from "../components/borderBox";
 
 export const Home = () => {
-
-    return (
-        <VPContainer>
-            <main className='home'>
-                <section className="section1">
-                    <VPBorderBox title='容量指标' className='bar'>
-                        <Charts1/>
-                    </VPBorderBox>
-                    <VPBorderBox title='发电单元类型分布' className='bar bar2'>
-                        <Charts2/>
-                    </VPBorderBox>
-                </section>
-                <section className="section2">
-                    <VPBorderBox title='负荷曲线' className='line'>
-                        <Charts3/>
-                    </VPBorderBox>
-                    <VPBorderBox title='考核指标' className='line'>
-                        <Charts4/>
-                    </VPBorderBox>
-                </section>
-                <section className="bordered section3">
-                    <h1>容量指标</h1>
-                    <Charts5/>
-                </section>
-                <section className="bordered section4">
-                    <h1>浙江省能源地图</h1>
-                    <Charts6/>
-                </section>
-                <section className="bordered section5">
-                    <h1>容量指标</h1>
-                    <Charts7/>
-                </section>
-                <section className="section6">
-                    <div className="bordered line" style={{marginBottom:px(10)}}>
-                        <h1>容量指标</h1>
-                        <Charts8/>
-                    </div>
-                    <div className="bordered line" style={{marginBottom:px(10)}}>
-                        <h1>运行模式</h1>
-                        <Charts9/>
-                    </div>
-                    <div className="bordered line">
-                        <h1>当日累计调控</h1>
-                        <Charts10/>
-                    </div>
-                </section>
-            </main>
-
-        </VPContainer>
-    );
+  return (
+    <VPContainer>
+      <main className="home">
+        <section className="section1">
+          <VPBorderBox title="容量指标" className="bar">
+            <Charts1 />
+          </VPBorderBox>
+          <VPBorderBox title="装机容量和发电量" className="bar bar2">
+            <Charts2 />
+          </VPBorderBox>
+        </section>
+        <section className="section2">
+          <VPBorderBox title="负荷曲线" className="line">
+            <Charts3 />
+          </VPBorderBox>
+          <VPBorderBox title="考核指标" className="line">
+            <Charts4 />
+          </VPBorderBox>
+        </section>
+        <section className="bordered section3">
+          <h1>容量指标</h1>
+          <Charts5 />
+        </section>
+        <section className="bordered section4">
+          <h1>浙江省能源地图</h1>
+          <Charts6 />
+        </section>
+        <section className="bordered section5">
+          <h1>容量指标</h1>
+          <Charts7 />
+        </section>
+        <section className="section6">
+          <div className="bordered line" style={{ marginBottom: px(10) }}>
+            <h1>容量指标</h1>
+            <Charts8 />
+          </div>
+          <div className="bordered line" style={{ marginBottom: px(10) }}>
+            <h1>运行模式</h1>
+            <Charts9 />
+          </div>
+          <div className="bordered line">
+            <h1>当日累计调控</h1>
+            <Charts10 />
+          </div>
+        </section>
+      </main>
+    </VPContainer>
+  );
 };
